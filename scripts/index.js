d20d4a938b35e7861a4bfdf598f397af6e89427e
AnnZblt <ann.zblt@yandex.ru>
2023-04-26 14:12:15 +0300

    Add lesson-7 hw

--- scripts/index.js
+++ scripts/index.js
@@ -4,12 +4,13 @@
 
 const goods = [
   {
-    index: 3,
+    index: 1,
     name: "Смартфон Xiaomi 11T 8/128GB",
     id: 1,
     description: "Смартфон Xiaomi 11T – это представитель флагманской линейки, выпущенной во второй половине 2021 года. И он полностью соответствует такому позиционированию, предоставляя своим обладателям возможность пользоваться отличными камерами, ни в чем себя не ограничивать при запуске игр и других требовательных приложений.",
     category: "mobile-phone",
-    discont: false,
+    discount: false,
+    discount_count: 0,
     units: "шт",
     count: 3,
     price: 27000,
@@ -20,12 +21,13 @@ const goods = [
     },
   },
   {
-    index: 4,
+    index: 2,
     name: "Радиоуправляемый автомобиль Cheetan",
     id: 2,
     description: "Внедорожник на дистанционном управлении. Скорость 25км/ч. Возраст 7 - 14 лет",
     category: "toys",
-    discont: 5,
+    discount: true,
+    discount_count: 5,
     units: "шт",
     count: 1,
     price: 4000,
@@ -36,12 +38,13 @@ const goods = [
     },
   },
   {
-    index: 5,
+    index: 3,
     name: "ТВ приставка MECOOL KI",
     id: 3,
     description: "Всего лишь один шаг сделает ваш телевизор умным, Быстрый и умный MECOOL KI PRO, прекрасно спроектированный, сочетает в себе прочный процессор Cortex-A53 с чипом Amlogic S905D",
     category: "tv-box",
-    discont: 15,
+    discount: true,
+    discount_count: 15,
     units: "шт",
     count: 4,
     price: 12400,
@@ -52,12 +55,13 @@ const goods = [
     },
   },
   {
-    index: 6,
+    index: 4,
     name: "Витая пара PROConnect 01-0043-3-25",
     id: 4,
     description: "Витая пара Proconnect 01-0043-3-25 является сетевым кабелем с 4 парами проводов типа UTP, в качестве проводника в которых используется алюминий, плакированный медью CCA. Такая неэкранированная витая пара с одножильными проводами диаметром 0.50 мм широко применяется в процессе сетевых монтажных работ. С ее помощью вы сможете обеспечить развертывание локальной сети в домашних условиях или на предприятии, объединить все необходимое вам оборудование в единую сеть.",
     category: "cables",
-    discont: false,
+    discount: false,
+    discount_count: 0,
     units: "v",
     count: 420,
     price: 22,
@@ -76,20 +80,30 @@ const modalForm = document.querySelector('.modal__form');
 const modalDiscountCheckBox = modalForm.querySelector('.modal__checkbox');
 const modalDiscountInput = modalForm.querySelector('.modal__input_discount');
 const bntAddGoods = document.querySelector('.panel__add-goods');
-const tableBody = document.querySelector('.table__body');
-const tableItem = document.querySelectorAll('.table__body tr').forEach(tr => {
+const goodsList = document.querySelector('.table__body');
+const goodsItem = document.querySelectorAll('.table__body tr').forEach(tr => {
   tr.classList.add('table__row', 'goods__item');
 });
+const vendorCodeId = overlayModal.querySelector('.vendor-code__id');
+let vendorCodeNumber = 0;
+const goodsTotalPrice = document.querySelector('.cms__total-price');
+const modalTotalPrice = overlayModal.querySelector('.modal__total-price');
+const modalGoodsCount = overlayModal.querySelector('.modal__label_count #count');
+const modalGoodsPrice = overlayModal.querySelector('.modal__label_price #price');
 
 overlay.classList.remove('active');
-tableBody.classList.add('goods__list');
+goodsList.classList.add('goods__list');
+
 
 //Goods creating
 
-const createRow = ({ index, id, name, category, units, count, price }) => {
+const createRow = ({ index, id, name, category, discount, discount_count, units, count, price }) => {
   const row = document.createElement('tr');
   row.classList.add('table__row', 'goods__item');
   row.dataset.index = index;
+  if (+discount === 1 || discount === 'on') {
+    price = price * ((100 - discount_count) / 100);
+  };
 
   row.innerHTML = `
   <tr>
@@ -100,8 +114,8 @@ const createRow = ({ index, id, name, category, units, count, price }) => {
     <td class="table__cell table__cell_left">${category}</td>
     <td class="table__cell">${units}</td>
     <td class="table__cell">${count}</td>
-    <td class="table__cell">$${price}</td>
-    <td class="table__cell">$${count * price}</td>
+    <td class="table__cell goods__price">$${price}</td>
+    <td class="table__cell goods__sum">$${count * price}</td>
     <td class="table__cell table__cell_btn-wrapper">
       <button class="table__btn table__btn_pic"></button>
       <button class="table__btn table__btn_edit"></button>
@@ -110,48 +124,164 @@ const createRow = ({ index, id, name, category, units, count, price }) => {
   </tr>
   `;
   return row;
-}
+};
 
 const renderGoods = (arr) => {
   let tableRow;
   arr.forEach((item) => {
     tableRow = createRow(item);
-    return tableBody.appendChild(tableRow);
+    return goodsList.appendChild(tableRow);
   });
-}
+};
 
 renderGoods(goods);
 
 
+// Find CMS total price 
+
+let allGoodsSum = document.getElementsByClassName('goods__sum');
+
+const findCmsTotalPrice = () => {
+  let itemsSum = 0;
+
+  for (let oneGoodsSum of allGoodsSum) {
+    let itemSum = oneGoodsSum.textContent.slice(1);
+    itemsSum += +itemSum;
+  }
+
+  goodsTotalPrice.textContent = '$ ' + itemsSum;
+
+};
+
+findCmsTotalPrice();
+
+
+// Modal control
+
+modalGoodsCount.addEventListener('blur', event => {
+  findModalTotalPrice(
+    modalTotalPrice,
+    modalGoodsCount.value,
+    modalGoodsPrice.value,
+    modalDiscountInput.value);
+});
+
+modalGoodsPrice.addEventListener('blur', event => {
+  findModalTotalPrice(
+    modalTotalPrice,
+    modalGoodsCount.value,
+    modalGoodsPrice.value,
+    modalDiscountInput.value);
+});
+
+modalDiscountInput.addEventListener('input', event => {
+  findModalTotalPrice(
+    modalTotalPrice,
+    modalGoodsCount.value,
+    modalGoodsPrice.value,
+    modalDiscountInput.value);
+});
+
+const discountAvailable = () => {
+  modalDiscountCheckBox.addEventListener('change', event => {
+    if (event.target.checked) {
+      modalDiscountInput.disabled = false;
+    } else {
+      modalDiscountInput.disabled = true;
+      modalDiscountInput.value = '';
+    }
+    findModalTotalPrice(
+      modalTotalPrice,
+      modalGoodsCount.value,
+      modalGoodsPrice.value,
+      modalDiscountInput.value);
+  })
+};
+
+const renderGoodsId = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  vendorCodeNumber = + Math.floor(Math.random() * (max - min + 1)) + min;
+
+  return vendorCodeNumber;
+};
+
+const findModalTotalPrice = (sum, count, price, discount) => {
+  sum.textContent = `$ ${count * (price * ((100 - discount) / 100))}`;
+};
+
+
 // Buttons behavior
 
-bntAddGoods.addEventListener('click', () => {
+const openModal = () => {
   overlay.classList.add('active');
-});
+  renderGoodsId(1, 10000000);
+  vendorCodeId.textContent = vendorCodeNumber;
+  discountAvailable();
+
+  return { vendorCodeNumber, vendorCodeId };
+};
+
+
+const closeModal = () => {
+  overlay.classList.remove('active');
+
+}
+
+bntAddGoods.addEventListener('click', openModal);
 
 overlay.addEventListener('click', event => {
   if (event.target === overlay || event.target.closest('.modal__close')) {
-    console.log(event.target);
-    overlay.classList.remove('active');
+    closeModal();
   }
 });
 
-tableBody.addEventListener('click', event => {
+goodsList.addEventListener('click', event => {
   const target = event.target;
 
   if (target.closest('.table__btn_del')) {
     const goodsItemId = +(target.closest('.goods__item').dataset.index);
-    console.log(goodsItemId);
     goods.forEach((item, i) => {
       if (item.index === goodsItemId) {
-        console.log(i);
         goods.splice(i, 1);
       }
-    }
-    );
+    });
 
     target.closest('.goods__item').remove();
+    findCmsTotalPrice();
 
     console.log(goods);
   }
 });
+
+
+// Add goods on page from modal
+
+const addGoodsData = item => {
+  goods.push(item);
+  console.log('Goods items: ', goods);
+  console.log(allGoodsSum);
+};
+
+const addGoodsItemPage = (goodsItem, goodsList) => {
+  goodsList.append(createRow(goodsItem));
+};
+
+const createGoods = (form, list, arr, id) => {
+  form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    formData.append('index', arr.length + 1);
+    formData.append('id', id.textContent);
+    const newGoodsItem = Object.fromEntries(formData);
+
+    addGoodsItemPage(newGoodsItem, list);
+    addGoodsData(newGoodsItem);
+    form.reset();
+    closeModal();
+    findCmsTotalPrice();
+  });
+};
+
+createGoods(modalForm, goodsList, goods, vendorCodeId);
